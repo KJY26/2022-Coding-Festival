8fa6905282dde06875f45a243ac2ae139dbaffa6
hyunjae000467 <36067070+hyunjae000467@users.noreply.github.com>
2022-11-05 02:21:59 +0900

    aa
    aa

--- MenuSelection.cpp
+++ MenuSelection.cpp
@@ -1,9 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <Windows.h>
 #include <stdlib.h>
 #include <conio.h>
-#define _CRT_SECURE_NO_WARNINGS
+#include <WinInet.h>
+#pragma comment(lib, "WinInet.lib")
+
 // 기본 정의 부분
+
+#define BUFF_SIZE 1024
+
+//인터넷 연결 확인 관련
+DWORD dwFlag;
+TCHAR szName[256];
 
 #define UP 0
 #define DOWN 1
@@ -11,11 +20,47 @@
 #define RIGHT 3
 #define SUBMIT 4 //Select for Enter
 //메뉴 함수 개발 부분
+
+char buff[BUFF_SIZE];
+FILE* fp;
+
+bool internet_status = false;
+
 int keyControl();
 void init();
 void titleDraw();
 int menuDraw();
 void gotoxy(int, int);
+void console();
+void remove_scrollbar();
+void add_scrollbar();
+bool InetStatus();
+
+void remove_scrollbar()
+{
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO info;
+	GetConsoleScreenBufferInfo(handle, &info);
+	COORD new_size =
+	{
+		info.srWindow.Right - info.srWindow.Left + 1,
+		info.srWindow.Bottom - info.srWindow.Top + 1
+	};
+	SetConsoleScreenBufferSize(handle, new_size);
+}
+
+void add_scrollbar()
+{
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO info;
+	GetConsoleScreenBufferInfo(handle, &info);
+	COORD new_size =
+	{
+		info.srWindow.Right + info.srWindow.Left + 1024,
+		info.srWindow.Bottom + info.srWindow.Top + 1024
+	};
+	SetConsoleScreenBufferSize(handle, new_size);
+}
 
 int keyControl() {
 	int key_value = _getch();
@@ -67,51 +112,123 @@ void gotoxy(int x, int y)
 	//커서 위치 이동 함수
 }
 
+void console() {
+	system("cls");
+	add_scrollbar();
+	while (1)
+	{
+		system("cls");
+		char message[1024];
+		gotoxy(0, 0);
+		printf("CONSOLE MODE |");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 2);//Green Text
+		printf(" ONLINE");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);//WHITE TEXT
+		printf("                      INPUT exit TO ESCAPE\n");
+		printf(" > ");
+		scanf("%s", message);
+		if (strcmp(message,"exit")==0)
+		{
+			gotoxy(0, 0);
+			printf("CONSOLE MODE |");
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);//RED Text
+			printf(" OFFLINE\n");
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);//WHITE TEXT
+			system("PAUSE");
+			break;
+		}
+		fp = _popen(message, "r");
+		if (NULL == fp) {
+			perror("Console Connection Failed");
+		}
+		while (fgets(buff, BUFF_SIZE, fp))
+		{
+			printf("%s", buff);
+		}
+		_pclose(fp);
+		printf("\n");
+		system("PAUSE");
+	}
+	remove_scrollbar();
+	return;
+}
+
+
 int menuDraw() {
 	int x = 27;
 	int y = 12;
-	gotoxy(x-2, y);
+	gotoxy(x - 2, y);
 	printf("> 테스트메뉴 1");
-	gotoxy(x, y+2);
+	gotoxy(x, y + 2);
 	printf("테스트메뉴 2");
-	gotoxy(x, y+4);
+	gotoxy(x, y + 4);
 	printf("테스트메뉴 3");
-	gotoxy(x, y+6);
-	printf("테스트메뉴 4");
+	gotoxy(x, y + 6);
+	printf("CONSOLE");
+	gotoxy(x, y + 8);
+	printf("인터넷 연결 재 확인");
 	while (1) {
 		int n = keyControl();
 		switch (n) {
-			case UP: {
-				if (y > 12)
-				{
-					gotoxy(x - 2, y);
-					printf(" ");
-					gotoxy(x - 2, y -= 2);
-					printf(">");
-				}
-				break;
+		case UP: {
+			if (y > 12)
+			{
+				gotoxy(x - 2, y);
+				printf(" ");
+				gotoxy(x - 2, y -= 2);
+				printf(">");
 			}
+			break;
+		}
 
-			case DOWN: {
-				if (y < 18) {
-					gotoxy(x - 2, y);
-					printf(" ");
-					gotoxy(x - 2, y += 2);
-					printf(">");
-				}
-				break;
+		case DOWN: {
+			if (y < 20) {
+				gotoxy(x - 2, y);
+				printf(" ");
+				gotoxy(x - 2, y += 2);
+				printf(">");
 			}
+			break;
+		}
 
-			case SUBMIT: {
-				return y - 12;
-			}
+		case SUBMIT: {
+			return y - 12;
+		}
 		}
 	}
 }
 
+bool InetStatus() {
+	system("cls");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+	printf("Universal Internet Connection Verification System\n");
+	printf("Version 1.00\n");
+	printf("\n");
+	if (::InternetGetConnectedStateEx(&dwFlag, szName, 256, 0)) {
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 2);//Green Text
+		printf(" ONLINE\n");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);//WHITE TEXT
+		system("PAUSE");
+		return true;
+	}
+	else {
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);//RED Text
+		printf("Connection FALSE\n");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);//WHITE TEXT
+		system("PAUSE");
+		return false;
+	}
+}
 int main() {
 	init();
 	titleDraw();
-	menuDraw();
+	while (1)
+	{
+		system("cls");
+		titleDraw();
+		int menuReturn = menuDraw();
+		if (menuReturn == 6) console();
+		if (menuReturn == 8) internet_status = InetStatus();
+	}
 	return 0;
 }
