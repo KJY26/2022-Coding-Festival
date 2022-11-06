ca3110cdfb384c6e54cf9e4f59704824673d526e
hyunjae000467 <36067070+hyunjae000467@users.noreply.github.com>
2022-11-06 23:05:24 +0900

    Version 3
    Added Internet Connection with Getting files from internet.
    No parsing file included

--- MenuSelection.cpp
+++ MenuSelection.cpp
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <WinInet.h>
+//#include "parson.h"
 #pragma comment(lib, "WinInet.lib")
 
 // 기본 정의 부분
@@ -14,12 +15,24 @@
 DWORD dwFlag;
 TCHAR szName[256];
 
+//메뉴 함수 개발 부분
 #define UP 0
 #define DOWN 1
 #define LEFT 2
 #define RIGHT 3
 #define SUBMIT 4 //Select for Enter
-//메뉴 함수 개발 부분
+
+//캘린더 파싱용 구조체 선언
+struct Event {
+	char* eventname;
+	int year;
+	int month;
+	int day;
+	bool first_grade;
+	bool second_grade;
+	bool third_grade;
+};
+struct Event evt[373];
 
 char buff[BUFF_SIZE];
 FILE* fp;
@@ -35,6 +48,8 @@ void console();
 void remove_scrollbar();
 void add_scrollbar();
 bool InetStatus();
+void Request_Calendar();
+//void Parse_Calendar();
 
 void remove_scrollbar()
 {
@@ -153,7 +168,6 @@ void console() {
 	return;
 }
 
-
 int menuDraw() {
 	int x = 27;
 	int y = 12;
@@ -202,26 +216,111 @@ bool InetStatus() {
 	system("cls");
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
 	printf("Universal Internet Connection Verification System\n");
-	printf("Version 1.00\n");
-	printf("\n");
-	if (::InternetGetConnectedStateEx(&dwFlag, szName, 256, 0)) {
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 2);//Green Text
-		printf(" ONLINE\n");
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);//WHITE TEXT
-		system("PAUSE");
-		return true;
+	if (internet_status == TRUE)
+	{
+		printf("이미 인터넷에 연결되어 있습니다. 다시 검사하시겠습니까?\n");
 	}
-	else {
+	if (internet_status == FALSE)
+	{
+		printf("현재 인터넷 연결 상태를 검사합니다.\n");
+	}
+	printf("\n");
+	int YN_return_value;
+	int x = 3;
+	int y = 3;
+	gotoxy(x - 2, y); //1
+	printf("> 인터넷 연결 검사");
+	gotoxy(x + 20, y); //13
+	printf("건너뛰기");
+	while (1) {
+		int n = keyControl();
+		switch (n) {
+		case RIGHT: {
+			if (x < 23) {
+				gotoxy(x - 2, y);
+				printf(" ");
+				x += 20;
+				gotoxy(x - 2, y);
+				printf(">");
+			}
+			break;
+		}
+		case LEFT: {
+			if (x > 3) {
+				gotoxy(x - 2, y);
+				printf(" ");
+				x -= 20;
+				gotoxy(x - 2, y);
+				printf(">");
+			}
+			break;
+		}
+
+		case SUBMIT: {
+			YN_return_value = x - 2;
+			goto cp1;
+		}
+		}
+	}
+cp1:
+	if (YN_return_value == 1) {
+		if (::InternetGetConnectedStateEx(&dwFlag, szName, 256, 0)) {
+			gotoxy(0, 4);
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 2);//Green Text
+			printf(" ONLINE(연결상태양호)\n");
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);//WHITE TEXT
+			return true;
+		}
+		else {
+			gotoxy(0, 4);
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);//RED Text
+			printf(" OFFLINE(연결상태불량)\n");
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);//WHITE TEXT
+			return false;
+		}
+	}
+	if (YN_return_value == 21) {
+		gotoxy(0, 4);
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);//RED Text
-		printf("Connection FALSE\n");
+		printf("\n오프라인 상태로 프로그램을 구동합니다.\n");
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);//WHITE TEXT
-		system("PAUSE");
 		return false;
 	}
 }
+
+void Request_Calendar() {
+	if (internet_status == TRUE)
+	{
+		system("curl -G \"https://open.neis.go.kr/hub/SchoolSchedule?ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010059\" -d \"KEY=c4d7533bcef9417097be47a4f311dd68&Type=xml&pIndex=1&pSize=300\" -o calendar.xml");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 2);//Green Text
+		printf("인터넷에서 정보를 가져오기를 성공하였습니다.\n");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);//WHITE TEXT
+		system("PAUSE");
+	}
+	else
+	{
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);//RED Text
+		printf("인터넷이 연결되지 않았으므로 정보 가져오기를 실패하였습니다.\n");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);//WHITE TEXT
+		system("PAUSE");
+	}
+}
+
+/*
+void Parse_Calendar() {
+	JSON_Value* rootValue;
+	JSON_Object* rootObject;
+	
+	rootValue = json_parse_file("calendar.json");
+	rootObject = json_value_get_object(rootValue);
+
+}
+*/
 int main() {
 	init();
 	titleDraw();
+	internet_status = InetStatus();
+	Request_Calendar();
 	while (1)
 	{
 		system("cls");
